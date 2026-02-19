832b573180608853c781650903393fc4bb9ed47d
Qiulin <rynnefan@umich.edu>
2026-02-18 19:18:00 -0500

    Ctrl X + Ctrl C 进入 normal mode normal mode 下空格键进入编辑模式 :wq 取代 :qa 保存退出

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,5 +1,8 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+
+vim.keymap.set("i", "<C-x><C-c>", "<Esc>", { desc = "Insert mode: Ctrl-X Ctrl-C to Normal mode" })
+vim.keymap.set("n", "<Space>", "i", { desc = "Normal mode: Space to Insert mode" })
+
+vim.cmd [[cnoreabbrev <expr> qa getcmdtype() == ':' && getcmdline() ==# 'qa' ? 'wq' : 'qa']]

76e5e534b1e377806e9e5feaee3728f05c1f5d40
Qiulin <rynnefan@umich.edu>
2026-02-18 19:22:01 -0500

    polish

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -6,3 +6,6 @@ vim.keymap.set("i", "<C-x><C-c>", "<Esc>", { desc = "Insert mode: Ctrl-X Ctrl-C 
 vim.keymap.set("n", "<Space>", "i", { desc = "Normal mode: Space to Insert mode" })
 
 vim.cmd [[cnoreabbrev <expr> qa getcmdtype() == ':' && getcmdline() ==# 'qa' ? 'wq' : 'qa']]
+
+local ok, ts_install = pcall(require, "nvim-treesitter.install")
+if ok then ts_install.prefer_git = true end
